bd8fb13414e8126b1c1defb78c1fb399943908fe
caoyongfeng0214 <caoyongfeng0214@gmail.com>
2020-04-09 04:14:40 +0800

    upgraded to v0.1.0

--- index.js
+++ index.js
@@ -1,103 +1,57 @@
-const _helper_setkey = function(obj, k) {
-    if(obj instanceof Object) {
-        obj.__$k = k;
-        let ks = Object.keys(obj);
-        for(let i = 0; i < ks.length; i++) {
-            let _k = ks[i];
-            let item = obj[_k];
-            _helper_setkey(item, k + (obj instanceof Array ? ('[' + _k + ']') : ('.' + _k)));
-        }
-    }
-};
-
-const _helper_getv = function(obj, keys, idx) {
-    if(idx < keys.length) {
-        let k = keys[idx];
-        let v = obj[k];
-        if(v) {
-            if(idx + 1 < keys.length) {
-                return _helper_getv(v, keys, idx + 1);
-            }
-            return v;
-        }
-    }
-    return undefined;
-};
-
-const _helper_getval = function(obj, k) {
-    let ary = k.split('.');
-    let ks = [];
-    ary.forEach((T) => {
-        if(T.indexOf('[') >= 0) {
-            let ary2 = T.split('[');
-            ary2.forEach((M) => {
-                ks.push(M.indexOf(']') > 0 ? parseInt(M) : M);
-            });
-        } else {
-            ks.push(T);
-        }
-    });
-    return _helper_getv(obj, ks, 0);
-};
-
-
 const app = getApp();
+
+const CHAR_DOT = '.';
+const CHAR_ARY = '[';
+
+const getV = function(o, k) {
+    if(k[0] === CHAR_ARY) {
+        return o[parseInt(k.substring(1, k.length - 1))];
+    } else {
+        return o[k.trim()];
+    }
+};
+
+const setV = function(o, k, v) {
+    if(k[0] === CHAR_ARY) {
+        o[parseInt(k.substring(1, k.length - 1))] = v;
+    } else {
+        o[k.trim()] = v;
+    }
+};
+
+const getKeys = function(key) {
+    let ary = [], k = '';
+    for(let i = 0; i < key.length; i++) {
+        let ch = key[i];
+        if(ch === CHAR_DOT || ch === CHAR_ARY) {
+            if(k) {
+                ary.push(k);
+                k = ch === CHAR_ARY ? ch : '';
+            } else {
+                throw 'illegal key: ' + key;
+            }
+        } else {
+            k += ch;
+        }
+    }
+    if(k) {
+        ary.push(k);
+    }
+    return ary;
+};
+
+const getObj = function(o, aryKeys) {
+    for(let i = 0; i < aryKeys.length - 1; i++) {
+        o = getV(o, aryKeys[i]);
+        if(o === undefined) {
+            throw aryKeys.join('') + ' is undefined';
+        }
+    }
+    return o;
+};
 
 
 function MiniAppService() {
-    let _data = {};
-    let _dataChange = function(target, property, descriptor) {
-        if(target === _data) {
-            _helper_setkey(descriptor.value, property);
-            _cmps.forEach((T) => {
-                T.setData({[T[_id] + '.' + property]: descriptor.value});
-            });
-        } else {
-            if(descriptor && descriptor.value) {
-                if(descriptor.value instanceof Object) {
-                    let _k = target.__$k;
-                    if(descriptor.value instanceof Array) {
-                        _k += '[' + property + ']';
-                    } else {
-                        _k += '.' + property;
-                    }
-                    _helper_setkey(descriptor.value, _k);
-                }
-                // let v = _helper_getval(_data, target.__$k);
-                // v[property] = descriptor.value;
-                // _cmps.forEach((T) => {
-                //     T.setData({[T[_id] + '.' + target.__$k]: v});
-                // });
-                setTimeout(() => {
-                    _cmps.forEach((T) => {
-                        T.setData({[T[_id] + '.' + target.__$k]: target});
-                    });
-                }, 0);
-            }
-        }
-    };
-    this.__defineGetter__('_$dataChange', function(target, property, descriptor) {
-        return _dataChange;
-    });
-    this.__defineSetter__('_$dataChange', function() {
-        throw '_$dataChange is readonly';
-    });
-    let _$data = MiniAppService.__watch(_data, _dataChange);
-    this.__defineGetter__('$data', function() {
-        return _$data;
-    });
-    this.__defineSetter__('$data', function() {
-        throw '$data is readonly';
-    });
-
-    let _cmps = [];
-    this.__defineGetter__('$cmps', function(target, property, descriptor) {
-        return _cmps;
-    });
-    this.__defineSetter__('$cmps', function() {
-        throw '$cmps is readonly';
-    });
-
     if(!MiniAppService.__id) {
         MiniAppService.__id = 1;
     }
@@ -108,54 +62,46 @@ function MiniAppService() {
     this.__defineSetter__('$id', function() {
         throw '$id is readonly';
     });
+    if(!app._$datas) {
+        app._$datas = {};
+    }
+    let _data = app._$datas[_id];
+    if(!_data) {
+        _data = app._$datas[_id] = {};
+    }
+    this.__defineGetter__('$data', function() {
+        return _data;
+    });
+    this.__defineSetter__('$data', function() {
+        throw '$data is readonly';
+    });
+
+    let _cmps = [];
+    this.__defineGetter__('$cmps', function() {
+        return _cmps;
+    });
+    this.__defineSetter__('$cmps', function() {
+        throw '$cmps is readonly';
+    });
 }
 
-MiniAppService.__watch = function(object, onChange) {
-    const handler = {
-        get(target, property, receiver) {
-            if(property === '__isProxy') {
-                return true;
-            }
-            if(property === '__$k' || !target[property] || target[property].__isProxy || target[property] instanceof Function || !(target[property] instanceof Object)) {
-                return target[property];
-            }
-            try {
-                return new Proxy(target[property], handler);
-            } catch (err) {
-                return Reflect.get(target, property, receiver);
-            }
-        },
-        defineProperty(target, property, descriptor) {
-            if(property !== '__$k') {
-                onChange(target, property, descriptor);
-            }
-            return Reflect.defineProperty(target, property, descriptor);
-        },
-        deleteProperty(target, property) {
-            if(property !== '__$k') {
-                onChange(target, property);
-            }
-            return Reflect.deleteProperty(target, property);
-        }
-    };
 
-    return new Proxy(object, handler);
-};
-
-MiniAppService.init = function(keyName, cmp) {
+MiniAppService.init = function(cmp) {
     if(!cmp) {
         throw 'cmp is undefined';
     }
     if(!cmp.setData) {
-        throw 'cmp is not Page or Component';
+        throw 'cmp is not a Page or Component';
+    }
+    if(!this.Name) {
+        throw 'service class missed "Name" property';
     }
     if(!app.__$instances) {
         app.__$instances = {};
     }
-    let instanceName = this.Name || keyName;
-    this.$instance = app.__$instances[instanceName];
+    this.$instance = app.__$instances[this.Name];
     if(!this.$instance) {
-        this.$instance = app.__$instances[instanceName] = new this();
+        this.$instance = app.__$instances[this.Name] = new this();
     }
     if(this.$instance.$cmps.indexOf(cmp) < 0) {
         if(cmp.$page) {
@@ -163,24 +109,17 @@ MiniAppService.init = function(keyName, cmp) {
                 cmp.$page.__$cmps = [];
             }
             cmp.$page.__$cmps.push(cmp);
-            let _onunload = cmp.$page.onUnload;
-            cmp.$page.onUnload = () => {
-                if(cmp.$page.__$cmps) {
-                    cmp.$page.__$cmps.forEach((T) => {
+        } else {
+            let _onunload = cmp.onUnload;
+            cmp.onUnload = () => {
+                if(cmp.__$cmps) {
+                    cmp.__$cmps.forEach((T) => {
                         let idx = this.$instance.$cmps.indexOf(T);
                         if(idx >= 0) {
                             this.$instance.$cmps.splice(idx, 1);
                         }
                     });
-                    delete cmp.$page.__$cmps;
-                    if(_onunload) {
-                        _onunload.apply(cmp.$page, []);
-                    }
                 }
-            };
-        } else {
-            let _onunload = cmp.onUnload;
-            cmp.onUnload = () => {
                 let idx = this.$instance.$cmps.indexOf(cmp);
                 if(idx >= 0) {
                     this.$instance.$cmps.splice(idx, 1);
@@ -191,15 +130,92 @@ MiniAppService.init = function(keyName, cmp) {
             };
         }
         this.$instance.$cmps.push(cmp);
-        cmp.setData({[keyName]: this.$instance.$data});
-        cmp[this.$instance.$id] = keyName;
+        cmp.setData({[this.Name]: this.$instance.$data});
+        cmp[this.$instance.$id] = this.Name;
     }
 
     return this.$instance;
 };
 
-MiniAppService.prototype.$set = function(key, val) {
-    this.$data[key] = val;
+
+MiniAppService.prototype.$apply = function() {
+    let k = arguments[0],
+        v = undefined,
+        filter = undefined;
+    if(k instanceof Function) {
+        filter = k;
+        k = undefined;
+    } else if (k instanceof Object) {
+        filter = arguments[1];
+        Object.keys(k).forEach((K) => {
+            k[this.Name + '.' + K] = k[K];
+            delete k[K];
+        });
+    } else if(k) {
+        v = arguments[1];
+        filter = arguments[2];
+        k = {
+            [this.constructor.Name + '.' + k]: v
+        };
+    }
+    if(!filter || filter instanceof Function) {
+        if(this.$cmps) {
+            if(!k) {
+                k = {[this.constructor.Name]: this.$data};
+            }
+            this.$cmps.forEach((T) => {
+                if(!filter || filter(T)) {
+                    T.setData(k);
+                }
+            });
+        }
+    }
+};
+
+
+MiniAppService.prototype.$set = function() {
+    let key = arguments[0],
+        filter = undefined;
+    if(key instanceof Object) {
+        filter = arguments[1];
+        Object.keys(key).forEach((T) => {
+            this.$set(T, key[T], filter);
+        });
+    } else {
+        let val = arguments[1];
+        filter = arguments[2];
+        let ary = getKeys(key);
+        let o = getObj(this.$data, ary);
+        setV(o, ary[ary.length - 1], val);
+        this.$apply(key, val, filter);
+    }
+};
+
+MiniAppService.prototype.$splice = function(key, val, filter) {
+    let ary = getKeys(key);
+    let o = getObj(this.$data, ary);
+    let list = o[ary[ary.length - 1]];
+    if(list instanceof Array) {
+        if(val instanceof Array) {
+            Array.prototype.splice.apply(list, val);
+            if(!filter || filter instanceof Function) {
+                if(this.$cmps) {
+                    let v = {
+                        [this.constructor.Name + '.' + key]: val
+                    };
+                    this.$cmps.forEach((T) => {
+                        if(!filter || filter(T)) {
+                            T.$spliceData(v);
+                        }
+                    });
+                }
+            }
+        } else {
+            throw 'val is not array';
+        }
+    } else {
+        throw key + ' is not array';
+    }
 };
 
 MiniAppService.prototype.$get = function(key) {
